ff612c4df44d95e944c12922c5a8029a6295f26f
jo55ef <jossef86@o2.pl>
2022-11-03 17:49:36 +0100

    mich

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //       2. Kazdy tworzy klase z nazwa jak swoje imie
 
 //        3. Niezaleznie zmieniamy imie w tym stringu na swoje
-        System.out.println("Siema jestem Jan");
+        System.out.println("Siema jestem Michal");
 
 //        4. Caly czas pracujemy razem ale jestesmy od siebie zalezni.
 //          Co chwile nam coś tu ktoś zmienia.

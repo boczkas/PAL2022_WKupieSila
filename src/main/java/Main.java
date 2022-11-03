ae233a85b258ec08306d96a1a497b08f146af1e7
jo55ef <jossef86@o2.pl>
2022-11-03 17:34:25 +0100

    mich

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Przemek2");
         System.out.println("Jan");
        System.out.println("Beata");
+        System.out.println("Michal");
 //       2. Kazdy tworzy klase z nazwa jak swoje imie
 
 //        3. Niezaleznie zmieniamy imie w tym stringu na swoje

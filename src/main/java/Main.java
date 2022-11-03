b33a18aa9b1e35305514503eafa59dd17ddf9177
Przemek <przemyslawjakubowski@gmail.com>
2022-11-03 12:53:21 +0100

    Lekcja 20

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+//        1. Kazdy dodaje swoje imie do wypisania
+        System.out.println("Siema tu:");
+        System.out.println("Przemek");
+
+//        2. Kazdy tworzy klase z nazwa jak swoje imie
+
+//        3. Niezaleznie zmieniamy imie w tym stringu na swoje
+        System.out.println("Siema jestem Przemek");
+
+//        4. Caly czas pracujemy razem ale jestesmy od siebie zalezni.
+//          Co chwile nam coś tu ktoś zmienia.
+//          Chcemy się uniezaleznic - branche
+
+//        5. Chcemy zeby ktos rzucil okiem na to czy nasz kod jest dobrze napisany - review
+    }
+}
